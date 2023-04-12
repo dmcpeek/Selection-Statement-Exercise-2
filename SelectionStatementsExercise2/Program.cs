a07188b21b1c817de742f40991204472f58db01f
Doug McPeek <dmcpeek@outlook.com>
2023-04-12 12:08:25 -0600

    First checkin

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Collections;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string subject;
+            Console.WriteLine("What is your favorite school subject?");
+            subject = Console.ReadLine();
+            string subjectToLower = subject.ToLower();
+
+            switch (subjectToLower) 
+            {
+                case "math":
+                    Console.WriteLine($"I'm not a big fan of {subjectToLower}.");
+                    break;
+                case "spelling":
+                    Console.WriteLine($"I guess {subjectToLower} is okay.");
+                    break;
+                case "history":
+                    Console.WriteLine($"I think {subjectToLower} should stay in the past.");
+                    break;
+                case "reading":
+                    Console.WriteLine($"I think {subjectToLower} is VERY important.");
+                    break;
+                case "biology":
+                    Console.WriteLine($"I think {subjectToLower} is AWESOME!!!.");
+                    break;
+                default:
+                    Console.WriteLine("I have no idea what that subject is. Do you?");
+                    break;
+            }
         }
     }
 }
